cde94bb551a43e055828112a59fc920faa42e55b
Mayk Brito <maykbrito@gmail.com>
2022-12-15 12:10:43 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
